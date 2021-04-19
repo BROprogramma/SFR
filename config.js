5538358bada1861a3c9c2e3eda5428ab6ebe7b0f
Arnoud de Boer <a.deboer@geonovum.nl>
2021-04-19 10:09:27 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,18 +2,27 @@ var respecConfig = {
   specStatus: "GN-WV",
   specType: "IM",
   //canonicalURI: "WV",
-  //publishDate: "2018-01-01",
+  publishDate: "2020-03-25",
   maxTocLevel: 4,
+ logos: [{
+    src: 'https://docs.geostandaarden.nl/bro/gen/logo_minbzk.png',
+    href: "https://www.geobasisregistraties.nl/basisregistraties/ondergrond",
+    alt: "Ministerie BZK",
+    width: 776,
+    height: 163,
+    id: 'logo_minbzk',
+  }]  ,	
   editors: [
-    {
-      name: "TNO Geologische Dienst Nederland",
-      url: "https://www.tno.nl/nl/",
-    },
     {
       name: "Geonovum",
       url: "https://www.geonovum.nl", 
     },  
   ],
+  authors: [
+    {
+      name: "Geonovum",
+	  url: "https://www.geonovum.nl",
+	}],
   shortName: "bro-sfr",
   pubDomain: "bro",
   github: "https://github.com/BROprogramma/SFR",
